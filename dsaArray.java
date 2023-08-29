266af4124ff2e866f74b2d584b57c83c96d3fb63
Abhinav Y <68462728+abhinav429@users.noreply.github.com>
2023-08-29 13:28:56 +0530

    Update dsaArray.java

--- dsaArray.java
+++ dsaArray.java
@@ -1,61 +1,6 @@
 import java.util.Scanner;
 public class dsaArray {
     public static void main(String[] args) {
-        // ****************************** Before Main method-  // 6)Prefix Sum of 2x2 matrix optimised approach
-    // first do prefix sum along row and then on that new matrix column sum and then on that matrix observe the pattern 
-    //of a particular element being equal to prefix sum of elements before it...Better to draw and visualise from here onwards 
-    // public static int sumRegion(int arr[][],int r1,int c1,int r2,int c2){
-    //     int sum,up,left,repeatedRegion,result=0;
-    //     sum=arr[r2][c2];
-    //     up=arr[r1-1][c2];
-    //     left=arr[r2][c1-1];
-    //     repeatedRegion=arr[r1-1][c1-1];
-    //     result=sum-up-left+repeatedRegion;
-    //     return result;
-    // }
-    // public static void prefixSumMatrix(int arr[][]){
-    //     int m=arr.length;
-    //     int n=arr[0].length;
-    //     // Row wise prefix-sum
-    //     for(int i=0;i<m;i++){
-    //         for(int j=1;j<n;j++){
-    //             arr[i][j]+=arr[i][j-1];
-    //         }
-    //     }
-    //     // Column wise prefix-sum
-    //     for(int j=0;j<n;j++){
-    //         for(int i=1;i<m;i++){
-    //             arr[i][j]+=arr[i-1][j];
-    //         }
-    //     }
-    // }    
-        // ************************   Next is after main method for 6) problem
-        // Scanner sc=new Scanner(System.in);
-        // System.out.println("Enter no of rows");
-        // int m=sc.nextInt();
-        // System.out.println("Enter no of columns");
-        // int n=sc.nextInt();
-        // int arr[][]=new int[m][n];
-        // System.out.println("Enter the matrix elements");
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         arr[i][j]=sc.nextInt();
-        //     }
-        // }
-        // int r1,c1,r2,c2;
-        // System.out.println("Enter r1 coordinate");
-        // r1=sc.nextInt();
-        // System.out.println("Enter c1 coordinate");
-        // c1=sc.nextInt();
-        // System.out.println("Enter r2 coordinate");
-        // r2=sc.nextInt();
-        // System.out.println("Enter c2 coordinate");
-        // c2=sc.nextInt();
-        // prefixSumMatrix(arr);
-        // int result=sumRegion(arr,r1,c1,r2,c2);
-        // System.out.println("Sum of elements of given rectangle is:"+result);
-        // sc.close();
-        
         // // 5)Rotate elements of matrix by 90 deg clockwise
         //     // Transpose elements then swap right and left columns then display
         // int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
